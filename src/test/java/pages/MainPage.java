1f91a872c037832a4a748892d664ce67b0e9fb01
ChesKnow <Roman.Chesanov@outlook.com>
2024-05-21 15:50:34 +0300

    Added AllureReport for main page

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -1,2 +1,51 @@
-package pages;public class MainPage {
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class MainPage {
+    //locators
+    private SelenideElement
+            title = $("#__next"),
+            trackingbar = $("#barcode"),
+            loop = $("[aria-label='Отследить']");
+
+    //actions
+    @Step("Открываем главную страницу")
+    public void openMainPageAndCheckTitle() {
+        open("");
+        title.shouldHave(text("Почта России"));
+    }
+    @Step("Открываем главную страницу и проверяем трекер через Enter")
+    public void trackingByPressEnter(String barcode) {
+        open("");
+        trackingbar.setValue(barcode).pressEnter();
+        $("#page-tracking").shouldHave(text(barcode));
+    }
+    @Step("Открываем главную страницу и проверяем трекер через лупу")
+    public void trackingByClickOnLoop(String barcode) {
+        open("");
+        trackingbar.setValue(barcode);
+        loop.click();
+        $("#page-tracking").shouldHave(text(barcode));
+    }
+    @Step("Открываем главную страницу и вводим неверный ШПИ - отслеживание не происходит")
+    public void trackingNotStartingTest(String barcode) {
+        open("");
+        trackingbar.setValue(barcode).pressEnter();
+        title.shouldHave(text("Почта России"));
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
 }
--- src/test/java/tests/ui/MainPageTests.java
+++ src/test/java/tests/ui/MainPageTests.java
@@ -1,2 +1,30 @@
-package tests.ui;public class MainPageTests {
+package tests.ui;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pages.MainPage;
+
+public class MainPageTests extends TestBase {
+
+    MainPage mainPage = new MainPage();
+
+    @Test
+    @DisplayName("Открываем главную страницу")
+    void openMainPageTest() {
+        mainPage.openMainPageAndCheckTitle();
+    }
+
+    @Test
+    @DisplayName("Проверяем трекер отправлений с главной страницы по нажатию Enter")
+    void trackingFromMainPageEnterTest() {
+        mainPage.trackingByPressEnter("RW052656168KZ");
+    }
+
+    @Test
+    @DisplayName("Проверяем трекер отправлений с главной страницы по клику на лупу")
+    void trackingFromMainPageLoopTest() {mainPage.trackingByClickOnLoop("RW052656168KZ");}
+
+    @Test
+    @DisplayName("Проверяем, что трекер не стартует если ввести неправильный ШПИ")
+    void trackingNotStartingTest() {mainPage.trackingNotStartingTest("RW052656168K");}
 }
